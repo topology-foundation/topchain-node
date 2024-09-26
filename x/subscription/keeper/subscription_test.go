e793833c9da5d325bf66f64cd2992eadba1afb40
Sacha Froment <sfroment42@gmail.com>
2024-09-26 18:52:50 +0200

    chore: add test to really test pagination
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>

--- x/subscription/keeper/subscription_test.go
+++ x/subscription/keeper/subscription_test.go
@@ -7,6 +7,8 @@ import (
 
 	"topchain/x/subscription/types"
 
+	query "github.com/cosmos/cosmos-sdk/types/query"
+
 	keepertest "topchain/testutil/keeper"
 )
 
@@ -50,5 +52,35 @@ func TestSubscriptions(t *testing.T) {
 	require.NoError(t, err)
 	require.Len(t, res.Subscriptions, 2)
 	require.Contains(t, res.Subscriptions, subscription1)
+	require.Contains(t, res.Subscriptions, subscription2)
+}
+
+func TestSubscriptionsWithPaginationOne(t *testing.T) {
+	keeper, ctx := keepertest.SubscriptionKeeper(t)
+	subscription1 := types.Subscription{
+		Id:       "sub1",
+		Provider: "provider1",
+	}
+	subscription2 := types.Subscription{
+		Id:       "sub2",
+		Provider: "provider1",
+	}
+	subscription3 := types.Subscription{
+		Id:       "sub3",
+		Provider: "provider2",
+	}
+
+	keeper.SetSubscription(ctx, subscription1)
+	keeper.SetSubscription(ctx, subscription2)
+	keeper.SetSubscription(ctx, subscription3)
+
+	req := &types.QuerySubscriptionsRequest{Provider: "provider1", Pagination: &query.PageRequest{Limit: 1}}
+	res, err := keeper.Subscriptions(ctx, req)
+	require.NoError(t, err)
+	require.Len(t, res.Subscriptions, 1)
+	require.Contains(t, res.Subscriptions, subscription1)
+	req = &types.QuerySubscriptionsRequest{Provider: "provider1", Pagination: &query.PageRequest{Key: res.Pagination.NextKey, Limit: 1}}
+	res, err = keeper.Subscriptions(ctx, req)
+	require.Len(t, res.Subscriptions, 1)
 	require.Contains(t, res.Subscriptions, subscription2)
 }
