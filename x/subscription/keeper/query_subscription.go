0b567128161f84cd06dfacbe5d534c3818b249d1
Sacha Froment <sfroment42@gmail.com>
2024-09-27 09:31:17 +0200

    chore: use key
    Co-authored-by: Jan Lewandowski <62848215+JanLewDev@users.noreply.github.com>

--- x/subscription/keeper/query_subscription.go
+++ x/subscription/keeper/query_subscription.go
@@ -33,7 +33,7 @@ func (k Keeper) Subscriptions(goCtx context.Context, req *types.QuerySubscriptio
 	}
 	ctx := sdk.UnwrapSDKContext(goCtx)
 	storeAdapter := runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx))
-	store := prefix.NewStore(storeAdapter, types.KeyPrefix(types.ProviderSubscriptionKeyPrefix+"/"+req.Provider))
+	store := prefix.NewStore(storeAdapter, types.KeyPrefix(types.SubscriptionProviderKeyPrefix+"/"+req.Provider))
 
 	var subscriptions []types.Subscription
 	pageRes, err := query.Paginate(store, req.Pagination, func(key []byte, value []byte) error {
