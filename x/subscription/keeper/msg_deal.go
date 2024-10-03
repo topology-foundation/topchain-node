85fd6cdc8c210b2239a2c77dd2e2ebecb02717d2
h3lio5 <akashsm@ce.iitr.ac.in>
2024-10-03 09:04:56 +0530

    refactor: move away from antipattern

--- x/subscription/keeper/msg_deal.go
+++ x/subscription/keeper/msg_deal.go
@@ -164,18 +164,17 @@ func (k msgServer) IncrementDealAmount(goCtx context.Context, msg *types.MsgIncr
 	if msg.Requester != deal.Requester {
 		return nil, errorsmod.Wrap(sdkerrors.ErrUnauthorized, "only the requester can increment the deal amount")
 	}
-	// cancelled or expired deals cannot be increment amount
-	// NOTE: due to the impl of EndBlock hook, deals can be incremented amount on their endblock.
-	if !k.IsDealUnavailable(deal.Status) {
-		sdkError := k.bankKeeper.SendCoinsFromAccountToModule(ctx, requester, types.ModuleName, sdk.NewCoins(sdk.NewInt64Coin("top", int64(msg.Amount))))
-		if sdkError != nil {
-			return nil, errorsmod.Wrap(sdkError, "failed to send coins to module account")
-		}
-		deal.TotalAmount += msg.Amount
-		deal.AvailableAmount += msg.Amount
-	} else {
+
+	if k.IsDealUnavailable(deal.Status) {
 		return nil, errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "cannot topup the expired deal with id "+msg.DealId)
 	}
+
+	sdkError := k.bankKeeper.SendCoinsFromAccountToModule(ctx, requester, types.ModuleName, sdk.NewCoins(sdk.NewInt64Coin("top", int64(msg.Amount))))
+	if sdkError != nil {
+		return nil, errorsmod.Wrap(sdkError, "failed to send coins to module account")
+	}
+	deal.TotalAmount += msg.Amount
+	deal.AvailableAmount += msg.Amount
 
 	k.SetDeal(ctx, deal)
 	return &types.MsgIncrementDealAmountResponse{}, nil
