9048466dab17f0cfea03cd1757384ab643742881
droak <me+git@droak.sh>
2024-11-29 15:48:04 +0900

    trim script

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -1,39 +1,28 @@
 #!/usr/bin/env bash
-echo "Formatting protobuf files"
-find ./ -name "*.proto" -exec clang-format -i {} \;
-
 set -e
-
 home=$PWD
 
-echo "Generating proto code"
 proto_dirs=$(find ./ -name 'buf.yaml' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   echo "Generating proto code for $dir"
 
   cd $dir
-  # check if buf.gen.pulsar.yaml exists in the proto directory
+
   if [ -f "buf.gen.pulsar.yaml" ]; then
     buf generate --template buf.gen.pulsar.yaml
-    # move generated files to the right places
     if [ -d "./api" ]; then
       cp -r ./api $home
       rm -rf ./api
     fi
   fi
 
-  # check if buf.gen.gogo.yaml exists in the proto directory
   if [ -f "buf.gen.gogo.yaml" ]; then
-      for file in $(find . -maxdepth 8 -name '*.proto'); do
-        # this regex checks if a proto file has its go_package set to cosmossdk.io/api/...
-        # gogo proto files SHOULD ONLY be generated if this is false
-        # we don't want gogo proto to run for proto files which are natively built for google.golang.org/protobuf
-        if grep -q "option go_package" "$file" && grep -H -o -c 'option go_package.*cosmossdk.io/api' "$file" | grep -q ':0$'; then
+      for file in $(find . -maxdepth 5 -name '*.proto'); do
+        if grep -q "option go_package" "$file"; then
           buf generate --template buf.gen.gogo.yaml $file
         fi
     done
 
-    # move generated files to the right places
     if [ -d "./mandu/x" ]; then
       cp -r ./mandu/x $home
       rm -rf ./mandu/x
